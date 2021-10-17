8cf1cbbe9f7d6d4fb78042d7096aebd12976bd24
kenwschen <ken00535@gmail.com>
2021-10-17 12:39:19 +0800

    feat(all): init commit

---
+++ src/app/note/note-item/note-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-item',
+  templateUrl: './note-item.component.html',
+  styleUrls: ['./note-item.component.css']
+})
+export class NoteItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
